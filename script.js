a7ec8c4999c6ac22722a73fb416c4b7834d94648
Son Nguyen Hoang <124531104+hoangsonww@users.noreply.github.com>
2023-04-11 22:32:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+const apikey = "3265874a2c77ae4a04bb96236a642d2f";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const buttonSearch = document.getElementById("btn");
+
+const url = (city) =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+async function getWeatherByLocation(city) {
+    const resp = await fetch(url(city), { origin: "cors" });
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    addWeatherToPage(respData);
+}
+
+function addWeatherToPage(data) {
+    const temp = KtoC(data.main.temp);
+
+    const weather = document.createElement("div");
+    weather.classList.add("weather");
+
+    weather.innerHTML = `
+        <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+        <small>${data.weather[0].main}</small>
+    `;
+
+    // cleanup
+    main.innerHTML = "";
+
+    main.appendChild(weather);
+}
+
+function KtoC(K) {
+    return Math.floor(K - 273.15);
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const city = search.value;
+
+    if (city) {
+        getWeatherByLocation(city);
+    }
+});
+
+buttonSearch.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = search.value;
+
+    if (city) {
+        getWeatherByLocation(city);
+    }
+});
+
+const heading = document.getElementById('my-heading');
+const subhead = document.getElementById('subhead');
+const colors = ['#ff0000',
+    '#00ff00',
+    '#0000ff',
+    '#ffff00',
+    '#00ffff',
+    '#ff00ff'];
+let timeoutID;
+
+function changeColor(event) {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    event.target.style.color = randomColor;
+    timeoutID = setTimeout(resetBackgroundColor, 500);
+}
+
+function resetColor(event) {
+    event.target.style.color = 'black';
+    clearTimeout(timeoutID);
+}
+
+function changeBackgroundColor(event) {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    event.target.style.backgroundColor = randomColor;
+    timeoutID = setTimeout(resetBackgroundColor, 500);
+}
+
+function resetBackgroundColor(event) {
+    event.target.style.backgroundColor = 'transparent';
+    clearTimeout(timeoutID);
+}
+
+heading.addEventListener('mouseover', changeColor);
+heading.addEventListener('mouseout', resetColor);
+buttonSearch.addEventListener('mouseover', changeBackgroundColor);
+buttonSearch.addEventListener('mouseout', resetBackgroundColor);
+search.addEventListener('mouseover', changeBackgroundColor);
+search.addEventListener('mouseout', resetBackgroundColor);
+subhead.addEventListener('mouseover', changeColor);
+subhead.addEventListener('mouseout', resetColor);
+main.addEventListener('mouseover', changeColor);
+main.addEventListener('mouseout', resetColor);
